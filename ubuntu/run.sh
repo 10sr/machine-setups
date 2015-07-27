bba8d16a3913aa90dc61885f6c09775294abe349
10sr <8slashes+git@gmail.com>
2015-07-27 11:02:30 +0900

    Add some basic files for ubuntu

---
+++ ubuntu/run.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+set -e
+
+if ! command -v ansible-playbook >/dev/null
+then
+    echo 'Command not found: ansible-playbook'
+    echo 'Install ansible first.'
+    exit 1
+fi
+
+if test $# -eq 0
+then
+    echo "usage: $0 <playbook> [...]"
+    exit 1
+fi
+
+ansible-playbook "$@"

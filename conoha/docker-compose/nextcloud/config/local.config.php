fb1ac2883ad0605091df37e902c354e4a266777f
10sr <8slashes+git@gmail.com>
2017-03-18 14:37:08 +0900

    Update nextcloud tasks

--- conoha/docker-compose/nextcloud/config/local.config.php
+++ conoha/docker-compose/nextcloud/config/local.config.php
@@ -1,5 +1,8 @@
 <?php
 $CONFIG = array(
+  'dbtype' => 'mysql',
+  'dbhost' => 'nextcloud0_mariadb0',
+
   'overwritehost' => '3ends.info',
   'overwriteprotocol' => 'https',
   'overwritewebroot' => '/nextcloud',

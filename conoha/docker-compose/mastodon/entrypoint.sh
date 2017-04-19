2781a821a1d4a067d7a0d282bcef3f248fe58ba5
10sr <8slashes+git@gmail.com>
2017-04-19 11:46:35 +0900

    Remove unused scripts

--- conoha/docker-compose/mastodon/entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-set -eux
-
-ionice -c 2 -n 7 nice -n 19 rails db:migrate
-ionice -c 2 -n 7 nice -n 19 rails assets:precompile
-exec ionice -c 2 -n 7 nice -n 19 rails s -p 3000 -b 0.0.0.0

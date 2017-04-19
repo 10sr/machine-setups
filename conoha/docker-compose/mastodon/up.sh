469aa5c644f62093a854f3d6433e4b411b786de1
10sr <8slashes+git@gmail.com>
2017-04-19 14:34:09 +0900

    Add minio support

--- conoha/docker-compose/mastodon/up.sh
+++ conoha/docker-compose/mastodon/up.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 set -eux
 
+set 2>&1
+
 nice ionice docker-compose run --rm web rails db:migrate
 nice ionice docker-compose run --rm web rails assets:precompile
 exec nice ionice docker-compose up --build

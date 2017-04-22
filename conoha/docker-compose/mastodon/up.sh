2ccc5589e80a309a5a28c7632340874ac941a566
10sr <8slashes+git@gmail.com>
2017-04-22 14:02:53 +0900

    Invoke docker-compose down before up

--- conoha/docker-compose/mastodon/up.sh
+++ conoha/docker-compose/mastodon/up.sh
@@ -3,6 +3,7 @@ set -eux
 
 set 2>&1
 
+docker-compose down
 nice ionice docker-compose run --rm web rails db:migrate
 nice ionice docker-compose run --rm web rails assets:precompile
 exec nice ionice docker-compose up --build

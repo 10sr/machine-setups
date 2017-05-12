819ad56053efc2556f46b9dcc4ea240096026b46
10sr <8slashes+git@gmail.com>
2017-05-12 14:36:24 +0900

    WIP
    ikiwiki0    | setlogsock(): type='unix': path not available at /usr/local/lib/perl5/site_perl/5.24.1/IkiWiki.pm line 839.
    ikiwiki0    | Died at /usr/local/lib/perl5/site_perl/5.24.1/IkiWiki/CGI.pm line 493.
    nginx0_1    | 172.19.0.1 - - [12/May/2017:05:33:49 +0000] "GET /ikiwiki/local.css HTTP/1.1" 200 1023 "http://localhost:8888/ikiwiki/" "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.10; rv:53.0) Gecko/20100101 Firefox/53.0" "-"

--- conoha/docker-compose/ikiwiki/setup.pl
+++ conoha/docker-compose/ikiwiki/setup.pl
@@ -3,20 +3,20 @@ require IkiWiki::Setup::Automator;
 our $wikiname = "10sr ikiwiki test";
 our $wikiname_short=IkiWiki::Setup::Automator::sanitize_wikiname($wikiname);
 our $rcs = "git";
-our $admin = "10sr ";
-our $domain = "wk.3ends.info";
+our $admin = "10sr";
+our $domain = "3ends.info";
 
 IkiWiki::Setup::Automator->import(
   wikiname => $wikiname,
   adminuser => [$admin],
   rcs => $rcs,
-  srcdir => "$ENV{HOME}/$wikiname_short",
-  destdir => "$ENV{HOME}/public_html/$wikiname_short",
+  srcdir => "/var/ikiwiki_src",
+  destdir => "/var/ikiwiki",
   repository => "$ENV{HOME}/$wikiname_short.".($rcs eq "monotone" ? "mtn" : $rcs),
   dumpsetup => "$ENV{HOME}/$wikiname_short.setup",
-  url => "http://$domain/~$ENV{USER}/$wikiname_short",
-  cgiurl => "http://$domain/~$ENV{USER}/$wikiname_short/ikiwiki.cgi",
-  cgi_wrapper => "$ENV{HOME}/public_html/$wikiname_short/ikiwiki.cgi",
+  url => "http://$domain/ikiwiki",
+  cgiurl => "http://$domain/ikiwiki/ikiwiki.cgi",
+  cgi_wrapper => "/var/ikiwiki/ikiwiki.cgi",
   adminemail => "$ENV{USER}\@$domain",
   add_plugins => [qw{goodstuff websetup}],
   disable_plugins => [qw{}],

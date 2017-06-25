10f5739c2640126eac3b1248f17df219a8be4ed5
10sr <8slashes+git@gmail.com>
2017-06-25 21:45:00 +0900

    Fixes

--- conoha/docker-compose/nextcloud/config/local.config.php
+++ conoha/docker-compose/nextcloud/config/local.config.php
@@ -1,9 +1,5 @@
 <?php
 $CONFIG = array(
-  'dbtype' => 'mysql',
-  'dbhost' => 'nextcloud0_mariadb0',
-
-  'overwritehost' => '3ends.info',
+  'overwritehost' => 'nc.3ends.info',
   'overwriteprotocol' => 'https',
-  'overwritewebroot' => '/nextcloud',
 );

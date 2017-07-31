ec736cd90412c0b6a58b310d0e47a7aeae6464e1
10sr <8slashes+git@gmail.com>
2017-07-31 14:24:36 +0900

    Remove nice ionice from mastodon up script

--- conoha/docker-compose/mastodon/up.sh
+++ conoha/docker-compose/mastodon/up.sh
@@ -4,9 +4,9 @@ set -eux
 set 2>&1
 
 docker-compose down
-nice ionice docker build  -t local/st.3ends.info \
+docker build  -t local/st.3ends.info \
      "https://github.com/10sr/mastodon.git#st.3ends.info"
-#nice docker-compose --verbose build --pull
-nice ionice docker-compose --verbose run --rm web rails db:migrate
-nice ionice docker-compose --verbose run --rm web rails assets:precompile
-exec nice ionice docker-compose --verbose up
+#docker-compose --verbose build --pull
+docker-compose --verbose run --rm web rails db:migrate
+docker-compose --verbose run --rm web rails assets:precompile
+exec docker-compose --verbose up

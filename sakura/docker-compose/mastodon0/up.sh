552e80336996db8fe26e369e1c2098fc7799c16a
10sr <8slashes+git@gmail.com>
2018-01-13 21:46:32 +0900

    Remove some comment

--- sakura/docker-compose/mastodon0/up.sh
+++ sakura/docker-compose/mastodon0/up.sh
@@ -7,8 +7,6 @@ docker-compose down
 docker build  -t local/st.3ends.info \
      "https://github.com/10sr/mastodon.git#st.3ends.info"
 #docker-compose --verbose build --pull
-# docker-compose --verbose run --rm web rails db:migrate
-# docker-compose --verbose run --rm web rails assets:precompile
 while true
 do
   timeout --preserve-status --signal=SIGTERM 30h docker-compose --verbose up

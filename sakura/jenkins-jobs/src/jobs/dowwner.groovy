3973a8cb7b0424762f64aa4be4e1940607a79340
10sr <8.slashes@gmail.com>
2019-01-25 15:24:10 +0900

    Add jobs

---
+++ sakura/jenkins-jobs/src/jobs/dowwner.groovy
@@ -0,0 +1,27 @@
+String basePath = 'dowwner'
+String repo = '10sr/dowwner'
+
+folder(basePath) {
+    description 'Dowwner job folder.'
+}
+
+job("$basePath/dowwner-check") {
+    scm {
+        github repo
+    }
+    triggers {
+        scm 'H/5 * * * *'
+    }
+    steps {
+        shell 'make installdeps check'
+    }
+}
+
+job("$basePath/dowwner-hoe") {
+    parameters {
+        stringParam 'host'
+    }
+    steps {
+        shell 'pwd && echo $host'
+    }
+}

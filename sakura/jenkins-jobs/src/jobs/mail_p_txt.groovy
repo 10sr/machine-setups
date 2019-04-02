da2a7815b9e49405efbc67d64b0a473f2e38e4ae
10sr <8.slashes@gmail.com>
2019-04-02 13:43:45 +0900

    Add BUILD_LOG

--- sakura/jenkins-jobs/src/jobs/mail_p_txt.groovy
+++ sakura/jenkins-jobs/src/jobs/mail_p_txt.groovy
@@ -12,9 +12,12 @@ job("mail_p_txt") {
     }
     publishers {
         extendedEmail {
+            // TODO: Set global sender address
             recipientList('8.slashes@gmail.com')
-            defaultSubject('$JOB_NAME')
-            // defaultContent('Something broken')
+            // defaultSubject('$JOB_NAME')
+            defaultContent('''$DEFAULT_CONTENT
+
+${BUILD_LOG}''')
             contentType('text/plain')
             triggers {
                 always {

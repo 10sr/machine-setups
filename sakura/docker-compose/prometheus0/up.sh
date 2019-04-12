28e02b180dd821227715b0012d1c2f40841d2852
10sr <8.slashes@gmail.com>
2019-04-12 19:39:29 +0900

    Add alertmanager email configs

--- sakura/docker-compose/prometheus0/up.sh
+++ sakura/docker-compose/prometheus0/up.sh
@@ -2,14 +2,23 @@
 set -eux
 
 . ../prometheus0.env
-export NODE_EXPORTER_PASSWORD
+#export NODE_EXPORTER_PASSWORD
 
 # prometheus image does not have envsubst command, so do this outside of container
-env \
+env -i - \
+    "NODE_EXPORTER_PASSWORD=$NODE_EXPORTER_PASSWORD" \
     envsubst \
-    '$NODE_EXPORTER_PASSWORD' \
     <./prometheus.yml.envsubst \
     >./prometheus.yml
+env -i - \
+    "AM_EMAIL_TO=$AM_EMAIL_TO" \
+    "AM_EMAIL_FROM=$AM_EMAIL_FROM" \
+    "AM_SMTP_SMARTHOST=$AM_SMTP_SMARTHOST" \
+    "AM_SMTP_USERNAME=$AM_SMTP_USERNAME" \
+    "AM_SMTP_PASSWORD=$AM_SMTP_PASSWORD" \
+    envsubst \
+    <./alertmanager.yml.envsubst \
+    >./alertmanager.yml
 
 
 exec docker-compose up --build

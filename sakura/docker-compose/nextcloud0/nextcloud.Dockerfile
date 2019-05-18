18ab36475f48f144bd61fa2efc98b2e0a9f43a99
10sr <8.slashes@gmail.com>
2019-05-18 21:06:52 +0900

    Do not build image for nextcloud containers

--- sakura/docker-compose/nextcloud0/nextcloud.Dockerfile
+++
@@ -1,8 +0,0 @@
-FROM nextcloud:14-fpm
-
-COPY start.sh /
-RUN chmod 755 /start.sh
-
-ENV NEXTCLOUD_UPDATE 1
-
-CMD ["/start.sh"]
--- sakura/docker-compose/nextcloud0/start.sh
+++ sakura/docker-compose/nextcloud0/start.sh
@@ -4,6 +4,7 @@ set -eux
 
 mkdir -p /var/www/html/config
 
+# Are these required?
 cp -pf /local.config.php /var/www/html/config/local.config.php
 cp -pf /redis.config.php /usr/src/nextcloud/config/redis.config.php
 

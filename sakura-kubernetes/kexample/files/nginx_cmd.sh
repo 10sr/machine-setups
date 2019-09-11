b2c36ea41c83b5c6f8dec1c725ff1ac2eb7cb0a9
10sr <8.slashes@gmail.com>
2019-09-11 13:39:08 +0900

    Update

--- sakura-kubernetes/kexample/files/nginx_cmd.sh
+++ sakura-kubernetes/kexample/files/nginx_cmd.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 set -eux
-cp -pf /configmap_volume/nginx_index.html /usr/share/nginx/html/index.html
+cp -pf $VOLUME_MOUNT_PATH/nginx_index.html /usr/share/nginx/html/index.html
 echo `hostname` >>/usr/share/nginx/html/index.html
 exec nginx -g "daemon off;"

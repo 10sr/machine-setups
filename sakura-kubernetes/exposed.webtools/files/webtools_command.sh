bb7dddb44aa378ec6319138035466713dafbf7b2
10sr <8.slashes@gmail.com>
2019-09-30 15:06:45 +0900

    Update webtools app

--- sakura-kubernetes/exposed.webtools/files/webtools_command.sh
+++ sakura-kubernetes/exposed.webtools/files/webtools_command.sh
@@ -1,8 +1,16 @@
 #!/bin/sh
 set -eux
 
+WEBTOOLS_SETTINGS_TOML=/settings.toml
+
 envsubst \
-    </settings.toml.envsubst \
+    <$WEBTOOLS_SETTINGS_TOML_ENVSUBST \
     >$WEBTOOLS_SETTINGS_TOML
 
-exec make migrate collectstatic gunicorn
+env
+
+cat $WEBTOOLS_SETTINGS_TOML
+
+while true; do sleep 1000; done
+
+#exec make migrate collectstatic gunicorn

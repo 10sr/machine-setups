9fd043dea3dca9065eefe9b10eea0052dea4f53b
10sr <8.slashes@gmail.com>
2019-10-01 12:15:50 +0900

    Use init contaienr to prepare settings_toml

--- sakura-kubernetes/exposed.webtools/files/webtools_init.sh
+++ sakura-kubernetes/exposed.webtools/files/webtools_init.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 set -eux
 
-export WEBTOOLS_SETTINGS_TOML=/settings.toml
-
 envsubst \
     <$WEBTOOLS_SETTINGS_TOML_ENVSUBST \
     >$WEBTOOLS_SETTINGS_TOML
@@ -10,7 +8,3 @@ envsubst \
 env
 
 cat $WEBTOOLS_SETTINGS_TOML
-
-# while true; do sleep 1000; done
-
-exec make migrate collectstatic gunicorn

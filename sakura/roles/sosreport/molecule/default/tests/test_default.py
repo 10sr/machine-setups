263a31e4f5501094ca10a1e1b372591664a2a32c
10sr <8.slashes@gmail.com>
2019-12-04 19:10:13 +0900

    Add molecule files for sosreport
    By runnint `pipenv run molecule init scenario -r sosreport` at roles/sosreport/.
    
    https://qiita.com/ysknkd/items/c738bb01f355dc378e94

---
+++ sakura/roles/sosreport/molecule/default/tests/test_default.py
@@ -0,0 +1,23 @@
+import os
+
+import testinfra.utils.ansible_runner
+
+testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
+    os.environ['MOLECULE_INVENTORY_FILE']
+).get_hosts('all')
+
+
+def test_hosts_file(host):
+    f = host.file('/etc/hosts')
+
+    assert f.exists
+    assert f.user == 'root'
+    assert f.group == 'root'
+    return
+
+
+def test_package(host):
+    f = host.package('sosreport')
+
+    assert f.is_installed
+    return

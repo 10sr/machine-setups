3425276d4877060263bc62512792a5ee195361e3
10sr <8.slashes@gmail.com>
2020-12-19 00:52:08 +0900

    Update

--- gcp-kubespray/gen_inventory.sh
+++ gcp-kubespray/gen_inventory.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 set -euxo pipefail
 
+# nodes_master=k8s-master02
+# nodes_node="k8s-node03 k8s-node04"
 nodes_master=k8s-master01
 nodes_node="k8s-node01 k8s-node02"
 # Assign three nodes

db7d3d5b5af943ec06344b3b7cf164cce49e24c6
10sr <8.slashes@gmail.com>
2023-10-03 22:48:14 +0900

    Fix instiki init

--- sakura-docker/docker-compose/instiki0/start.sh
+++ sakura-docker/docker-compose/instiki0/start.sh
@@ -5,4 +5,10 @@ set -eux
     #      >>./config/environments/production.rb
 patch -p1 </secure_cookie.patch
 
+# ?
+# For error:
+# 0|instiki0 | instiki0 | /usr/local/lib/ruby/site_ruby/2.6.0/bundler/source/git.rb:221:in `rescue in load_spec_files': https://github.com/distler/maruku.git (at nokogiri@d56b03f) is not yet checked out. Run `bundle install` first. (Bundler::GitError)
+# https://stackoverflow.com/questions/6648870/is-not-checked-out-bundle-install-does-not-fix-help
+bundle install --deployment
+
 exec "$@"
